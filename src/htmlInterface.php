1e1be6e51aa3b71b74f47fe9e81b0dcb42de82e3
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-07 00:32:54 +0000

    full html redesign
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/htmlInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Soldy\retard;
+
+interface htmlInterface
+{
+    public function addJs(string &$url) : bool
+    public function addCss(string &$url) : bool
+    public function addMeta(array &$meta) : bool
+    public function addToBody(string &$element) : bool
+    public function changeTitle(string &$element) : bool
+}
