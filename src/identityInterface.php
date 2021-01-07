361716534f4a3991f3aee7be5adc1e5f34575029
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-07 00:36:28 +0000

    parent child logic
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- src/identityInterface.php
+++ src/identityInterface.php
@@ -5,4 +5,8 @@ namespace Soldy\retard;
 interface identityInterface
 {
     public function getId() : string
+    public function getParent() : string
+    public function setParent(string &$parent) : bool
+    public function getChilds() : array
+    public function addChild(string &$child) : bool
 }
