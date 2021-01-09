e08e586d93365ad26c4749fb1391388658f9fd94
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-09 19:42:14 +0000

    besic element manager
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/element.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Soldy\retard;
+
+use Soldy\retard\ElementInterface;
+
+class Element implements ElementInterface
+{
+    private $separetor = '"'; // please fdo not extend to psr12
+    private function attributionsFull(array &$attributions) :string
+    {
+        $out = '';
+        foreach ($attributions as $attr => $val){
+            $out.= (
+                ' '.
+                $att.
+                '='.
+                $this->attributionValue($val)
+            );
+        }
+        return (
+            $out
+        );
+    }
+    private function attributionValue(array &$val) : string
+    {
+        $out = $this->separetor;
+        $out.= $val[0];
+        for ($i = 1 ; count($val)>$i;$i++){
+             $out.= ' '.$val[$i];
+        }
+        $out .= $this->separetor;
+        return $out;
+    }
+    public function multi (
+         string &$element = 'div',
+         array  &$attributions = [],
+         string &$inside = ''
+    ) : string
+    {
+        return ( 
+            '<'.
+            $element.
+            $this->attributionFull($attributions).
+            '>'.
+            $inside.
+            '</'. 
+            $element. 
+            '>'
+        );
+
+    }
+    public function single (
+         string &$element,
+         array  &$attributions,
+    ) : string
+    {
+        return (
+            '<'.
+            $element.
+            $this->attributionFull($attributions).
+            '/>'
+        );
+    }
+
+
+
+}
+
+
